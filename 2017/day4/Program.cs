7cb9f05559771185af9952e0447b766a58aedeb1
Dagur Páll Ammendrup <dagur.ammendrup@advania.is>
2017-12-04 09:22:15 +0000

    day 4

---
+++ 2017/day4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day4
+{
+    class Program
+    {
+
+        static string[][] readFile(string filename) =>
+            File.ReadLines(filename).Select((line) => line.Split(" ")).ToArray();
+
+        static void Main(string[] args)
+        {
+            var input = readFile("input.txt");
+
+            var part1 = input.Where(line => line.Distinct().Count() == line.Count()).Count();
+            Console.WriteLine(part1);
+
+            var part2 = input.Select(line => line.Select(s => string.Join("", s.ToCharArray().OrderBy(c => c))))
+                             .Where(line => line.Distinct().Count() == line.Count()).Count();
+            Console.WriteLine(part2);            
+        }
+    }
+}
