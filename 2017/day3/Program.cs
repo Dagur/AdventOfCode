7f1b3b08f7699bd18cb01297af43474feb9da17c
Dagur Páll Ammendrup <dagurp@gmail.com>
2017-12-04 21:23:51 +0000

    Day 3 part 2

--- 2017/day3/Program.cs
+++ 2017/day3/Program.cs
@@ -12,6 +12,7 @@ namespace day3
             public int distance;
         }
 
+        /*Damn whomever decided to use count as the second argument */
         static IEnumerable<int> Range(int start, int end)
         {
             return Enumerable.Range(start, end - start);
@@ -20,7 +21,7 @@ namespace day3
         static IEnumerable<Square> CreateSquares(int maxval)
         {
             int val = 1;
-            int circle = 1;            
+            int circle = 1;
 
             yield return new Square { val = val++, distance = 0 };
             while (val < maxval)
@@ -33,8 +34,52 @@ namespace day3
                 {
                     yield return new Square { val = val++, distance = distance };
                 }
-                circle++;                
+                circle++;
             }
+        }
+
+        //part 2
+        public static int[][] GenerateEmptyMatrix(int size)
+        {
+            return Range(0, size).Select(_ =>
+                Range(0, size).Select(__ => (int)0).ToArray()
+            ).ToArray();
+        }
+
+        public static IEnumerable<(int x, int y)> Moves()
+        {
+            var (x, y) = (0, 0);
+            var layer = 0;
+            //yield return (x, y); Skip the center
+            while (true)
+            {
+                layer += 1;
+                yield return (++x, y);
+                while (y < layer)
+                {
+                    yield return (x, ++y);
+                }
+                while (x > -layer)
+                {
+                    yield return (--x, y);
+                }
+                while (y > -layer)
+                {
+                    yield return (x, --y);
+                }
+                while (x < layer)
+                {
+                    yield return (++x, y);
+                }
+            }
+        }
+
+        static int GetSquareSum(int[][] matrix, (int x, int y) point)
+        {
+            return Range(point.x-1,point.x+2)
+                .Select(x => Range(point.y-1, point.y+2).Select(y => matrix[x][y]))
+                .SelectMany(i=>i)
+                .Sum();
         }
 
         static void Main(string[] args)
@@ -43,6 +88,21 @@ namespace day3
             var part1 = CreateSquares(input).Where(square => square.val == input).First();
             Console.WriteLine(part1.distance);
 
+            //Part 2
+            var matrix = GenerateEmptyMatrix(99);
+            (int x, int y) center = (44, 44);
+            matrix[center.x][center.y] = 1;
+
+            foreach ((int x, int y) point in Moves().Select(m => (m.x + center.x, m.y + center.y)))
+            {
+                var val = GetSquareSum(matrix, point);
+                matrix[point.x][point.y] = val;
+
+                if (val > input) {
+                    Console.WriteLine($"({point.x},{point.y}): {val}");
+                    break;
+                }
+            }
         }
     }
 }
