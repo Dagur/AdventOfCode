b931b23dda2bbf9e49ea438c29fbfa4d0343d63c
Dagur Páll Ammendrup <dagur.ammendrup@advania.is>
2017-12-05 12:30:33 +0000

    Day 5

---
+++ 2017/day5/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace day5
+{
+    class Program
+    {
+        static int Jump(int[] offsets)
+        {
+            int jumpCount = 0;
+            int position = 0;
+            int nextpos = 0;
+            while (nextpos < offsets.Length) {
+                nextpos = position + offsets[position]++;
+                position = nextpos;
+                jumpCount++;
+            }
+            return  jumpCount;
+        }
+
+        static int Jump2(int[] offsets)
+        {
+            int jumpCount = 0;
+            int position = 0;
+            int nextpos = 0;
+            while (nextpos < offsets.Length) {
+                if (offsets[position] < 3) {
+                    nextpos = position + offsets[position]++;
+                } else {
+                    nextpos = position + offsets[position]--;
+                }                
+                position = nextpos;
+                jumpCount++;
+            }
+            return  jumpCount;
+        }
+
+        static void Main(string[] args)
+        {
+            var input = File.ReadLines("input.txt").Select(Int32.Parse);
+            Console.WriteLine(Jump(input.ToArray()));
+            Console.WriteLine(Jump2(input.ToArray()));
+        }
+    }
+}
