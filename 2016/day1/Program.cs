478c7d4afc7a82b885a90018396301af94635f63
Dagur Páll Ammendrup <dagurp@gmail.com>
2017-12-06 20:20:06 +0000

    Finally finished day 1

--- 2016/day1/Program.cs
+++ 2016/day1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace day1
@@ -9,15 +10,16 @@ namespace day1
         struct Position
         {
             public Direction facing;
-            public double x;
-            public double y;
+            public int x;
+            public int y;
         }
-        static double measureDistance(Position startPos, Position endPos)
+
+        static int measureDistance(Position startPos, Position endPos)
         {
             return Math.Abs(endPos.x - startPos.x) + Math.Abs(endPos.y - startPos.y);
         }
 
-        static double moveX(Direction dir, double distance)
+        static int moveX(Direction dir, int distance)
         {
             switch (dir)
             {
@@ -30,7 +32,7 @@ namespace day1
             }
         }
 
-        static double moveY(Direction dir, double distance)
+        static int moveY(Direction dir, int distance)
         {
             switch (dir)
             {
@@ -43,9 +45,9 @@ namespace day1
             }
         }
 
-        static Position move(Position pos, string instruction)
+        static Position move(Position pos, (char, int) instruction)
         {
-            (char side, double distance) = (instruction[0], int.Parse(instruction.Substring(1)));
+            var (side, distance) = instruction;
             var facing = (uint)pos.facing;
             var direction = (Direction)((side == 'R' ? facing + 1 : facing - 1) % 4);
 
@@ -56,9 +58,24 @@ namespace day1
                 y = pos.y + moveY(direction, distance)
             };
         }
+
+        static Direction GetDirection(Direction facing, char turn)
+        {
+            return (Direction)((turn == 'R' ? (uint)facing + 1 : (uint)facing - 1) % 4);
+        }
+
         static void Main(string[] args)
         {
-            const string instructions = "R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5, R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2, R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4, L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3, R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2, R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1, L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5, L1, R2, L5, L2, L3, R2, L2";
+            const string instructions = "R2, L3, R2, R4, L2, L1, R2, R4, R1, L4, L5, R5, R5" +
+            ", R2, R2, R1, L2, L3, L2, L1, R3, L5, R187, R1, R4, L1, R5, L3, L4, R50, L4, R2" +
+            ", R70, L3, L2, R4, R3, R194, L3, L4, L4, L3, L4, R4, R5, L1, L5, L4, R1, L2, R4" +
+            ", L5, L3, R4, L5, L5, R5, R3, R5, L2, L4, R4, L1, R3, R1, L1, L2, R2, R2, L3, R3" +
+            ", R2, R5, R2, R5, L3, R2, L5, R1, R2, R2, L4, L5, L1, L4, R4, R3, R1, R2, L1, L2" +
+            ", R4, R5, L2, R3, L4, L5, L5, L4, R4, L2, R1, R1, L2, L3, L2, R2, L4, R3, R2, L1" +
+            ", L3, L2, L4, L4, R2, L3, L3, R2, L4, L3, R4, R3, L2, L1, L4, R4, R2, L4, L4, L5" +
+            ", L1, R2, L5, L2, L3, R2, L2";
+            
+            var moves = instructions.Split(", ").Select(x => (x[0], int.Parse(x.Substring(1)))).ToArray();
             var startPos = new Position
             {
                 facing = Direction.North,
@@ -66,8 +83,51 @@ namespace day1
                 y = 0
             };
 
-            var endPos = instructions.Split(", ").Aggregate(startPos, move);
+            var endPos = moves.Aggregate(startPos, move);
             Console.WriteLine(measureDistance(startPos, endPos));
+
+            /* Part 2 */
+            var expandedMoves = moves.Aggregate(new List<Direction>(), (moveList, instruction) =>
+            {
+                var (turn, distance) = instruction;
+                var direction = GetDirection(moveList.LastOrDefault(), turn);
+                while (distance-- > 0)
+                {
+                    moveList.Add(direction);
+                }
+                return moveList;
+            });
+
+            var history = new List<(int, int)> { (0, 0) };
+            (int x, int y) pos = (0, 0);
+            (int x, int y) nextpos = (0, 0);
+            foreach (var move in expandedMoves)
+            {
+                switch (move)
+                {
+                    case Direction.North:
+                        nextpos = (pos.x, pos.y + 1);
+                        break;
+                    case Direction.East:
+                        nextpos = (pos.x + 1, pos.y);
+                        break;
+                    case Direction.South:
+                        nextpos = (pos.x, pos.y - 1);
+                        break;
+                    case Direction.West:
+                        nextpos = (pos.x - 1, pos.y);
+                        break;
+                }
+                if (history.Any(((int x, int y) p) => p.x == nextpos.x && p.y == nextpos.y))
+                {
+                    endPos = new Position { facing = move, x = nextpos.x, y = nextpos.y };
+                    Console.Write(measureDistance(startPos, endPos));
+                    break;
+                }
+                history.Add(nextpos);
+                pos = nextpos;
+
+            }
         }
     }
 }
