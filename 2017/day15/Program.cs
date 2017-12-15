c653f4ae25b45527ceb15127d62e2c5d6d2041d5
Dagur Páll Ammendrup <dagurp@gmail.com>
2017-12-15 22:05:02 +0000

    Optimized using stolen ideas from reddit

--- 2017/day15/Program.cs
+++ 2017/day15/Program.cs
@@ -6,7 +6,7 @@ namespace day15
 {
     class Program
     {
-        static IEnumerable<ulong> Generator(ulong startVal, ulong factor, ulong divider)
+        static IEnumerable<long> Generator(long startVal, long factor, long divider)
         {
             var last = startVal;
             while (true)
@@ -16,52 +16,45 @@ namespace day15
             }
         }
 
-        static IEnumerable<ulong> MultiplesOf(IEnumerable<ulong> seq, int val)
+        static IEnumerable<long> MultiplesOf(IEnumerable<long> seq, int val)
         {
             return seq.Where(x => (long)x % val == 0);
         }
 
-        static bool BinCompare(ulong A, ulong B)
+        static bool BinCompare(long A, long B)
         {
-            var testa = Convert.ToString((long)A, 2);
-            var testb = Convert.ToString((long)B, 2);
-            return testa.Substring(Math.Max(0, testa.Length - 16)).Equals(testb.Substring(Math.Max(0, testb.Length - 16)));
+            return (A & 0xFFFF) == (B & 0xFFFF);
         }
 
-        static int CountMatches(int pairsToConsider, IEnumerator<ulong> genA, IEnumerator<ulong> genB)
+        static int CountMatches(int pairsToConsider, IEnumerable<long> genA, IEnumerable<long> genB)
         {
-            return Enumerable.Range(0, pairsToConsider)
-                .Select(_ =>
-                {
-                    genA.MoveNext();
-                    genB.MoveNext();
-                    return (genA.Current, genB.Current);
-                })
+            return genA.Zip(genB, (a, b) => (a, b))
+                .Take(pairsToConsider)
                 .AsParallel()
-                .Where(p => BinCompare(p.Item1, p.Item2))
+                .Where(p => BinCompare(p.Item1, p.Item2))                
                 .Count();
         }
 
         static void Main(string[] args)
         {
-            ulong A = 277;
-            ulong B = 349;
-            ulong factorA = 16807;
-            ulong factorB = 48271;
-            ulong divider = 2147483647;
+            long A = 277;
+            long B = 349;
+            long factorA = 16807;
+            long factorB = 48271;
+            long divider = 2147483647;
 
-            var genA = Generator(A, factorA, divider).GetEnumerator();
-            var genB = Generator(B, factorB, divider).GetEnumerator();
+            var genA = Generator(A, factorA, divider);
+            var genB = Generator(B, factorB, divider);
 
             var part1 = CountMatches(40000000, genA, genB);
             Console.WriteLine(part1);
 
             // part 2
 
-            var genAm = MultiplesOf(Generator(A, factorA, divider), 4).GetEnumerator();
-            var genBm = MultiplesOf(Generator(B, factorB, divider), 8).GetEnumerator();
+            var genAm = MultiplesOf(Generator(A, factorA, divider), 4);
+            var genBm = MultiplesOf(Generator(B, factorB, divider), 8);
 
-            var part2 = CountMatches(5000000, genAm, genBm);            
+            var part2 = CountMatches(5000000, genAm, genBm);
             Console.WriteLine(part2);
         }
     }
