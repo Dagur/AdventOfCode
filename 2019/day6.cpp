20ed5ff8ddedd57823318218a53aeffc20fce09b
Dagur Páll Ammendrup <dagurp@gmail.com>
2019-12-06 21:12:25 +0000

    Day 6 complete

--- 2019/day6.cpp
+++ 2019/day6.cpp
@@ -1,69 +1,90 @@
 #include <unordered_map>
 #include <numeric>
 #include <algorithm>
+#include <iterator>
 #include "utils/input.cpp"
 
 using namespace std;
 
-pair<string, string> split(const string orbit)
+using Tree = unordered_map<string, vector<string>>;
+
+pair<string, string> split_up(const string &orbit)
 {
     const int delim = orbit.find(')');
     return {orbit.substr(0, delim), orbit.substr(delim + 1)};
 }
 
-void insert_or_update(unordered_map<string, int> &map, const string key, const int increment)
+Tree construct_tree(const vector<string> &input)
 {
-    // auto it = map.find(key);
-    // if (it != map.end())
-    // {
-        map[key] += increment;
-    // }
-    // else
-    // {
-    //     map.emplace(key, increment);
-    // }
-}
-
-int sum(const unordered_map<string, int> &map)
-{
-    return accumulate(
-        map.begin(),
-        map.end(),
-        0,
-        [](int value, const unordered_map<string, int>::value_type &p) { return value + p.second; });
-}
-
-int part1(vector<string> input)
-{
-    unordered_map<string, int> objects;
-    sort(input.begin(), input.end());
+    Tree objects;
 
     for (string orbit : input)
     {
-        auto couple = split(orbit);
-        insert_or_update(objects, couple.second, 1 + objects[couple.first]);
+        auto couple = split_up(orbit);
+        objects[couple.second]; // Make sure that leaves exist on the tree
+        objects[couple.first].push_back(couple.second);
     }
 
-    return sum(objects);
+    return objects;
+}
+
+int count_orbits(const Tree &tree, const string &start_node, const int depth = 0)
+{
+    int val = depth;
+    for (auto child : tree.at(start_node))
+    {
+        val += count_orbits(tree, child, depth + 1);
+    }
+    return val;
+}
+
+vector<string> find_the_way(const Tree &tree, const string &target, string start_at = "COM")
+{
+    auto children = tree.at(start_at);
+    for (auto child : children)
+    {
+        if (child == target)
+        {
+            return {child};
+        }
+        vector<string> acc = find_the_way(tree, target, child);
+        if (acc.size() > 0)
+        {
+            acc.push_back(child);
+            return acc;
+        }
+    }
+    return {};
+}
+
+int part1(const Tree &tree)
+{
+    return count_orbits(tree, "COM");
+}
+
+int part2(const Tree &tree)
+{
+    auto path_1 = find_the_way(tree, "YOU");
+    auto path_2 = find_the_way(tree, "SAN");
+
+    vector<string>::iterator it2;
+    for (auto it(path_1.begin()); it != path_1.end(); ++it)
+    {
+        if ((it2 = find(path_2.begin(), path_2.end(), *it)) != path_2.end())
+        {
+            int d1 = distance(path_2.begin(), it2) - 1;
+            int d2 = distance(path_1.begin(), it) - 1;
+            return d1 + d2;
+        }
+    }
+    return 0;
 }
 
 int main()
 {
     const vector<string> input = read_input_vs("input/6/6.txt");
+    const Tree tree = construct_tree(input);
 
-    cout << "Part 1: " << part1({
-                              "S)B",
-                            "B)G",
-                              "G)H",
-                              "D)I",
-                              "E)J",
-                              "J)K",
-                              "K)L",
-                              "B)C",
-                              "C)D",
-                              "D)E",
-                              "E)F",
-
-                          })
-         << endl;
+    cout << "Part 1: " << part1(tree) << endl;
+    cout << "Part 2: " << part2(tree) << endl;
 }
