2218ee164009695ccc6e8da4ebdf7bbc1ec18b85
Dagurmart <dagur@hausmart.com>
2019-12-19 13:33:01 +0000

    Day 12 Part 1

--- 2019/build.sh
+++ 2019/build.sh
@@ -5,13 +5,14 @@
 g++ -c *.cpp utils/*.cpp
 g++ input.o day1.o -o build/day1
 g++ input.o day2.o -o build/day2
-g++ input.o day3.o -o build/day3 
-g++ input.o day4.o -o build/day4 
-g++ intcode.o input.o day5.o -o build/day5 
-g++ input.o day6.o -o build/day6 
-g++ intcode.o input.o day7.o -o build/day7 
-g++ input.o day8.o -o build/day8 
-g++ intcode.o input.o day9.o -o build/day9 
-g++ input.o day10.o -o build/day10 
-g++ intcode.o input.o day11.o -o build/day11 
+g++ input.o day3.o -o build/day3
+g++ input.o day4.o -o build/day4
+g++ intcode.o input.o day5.o -o build/day5
+g++ input.o day6.o -o build/day6
+g++ intcode.o input.o day7.o -o build/day7
+g++ input.o day8.o -o build/day8
+g++ intcode.o input.o day9.o -o build/day9
+g++ input.o day10.o -o build/day10
+g++ intcode.o input.o day11.o -o build/day11
+g++ input.o day12.o -o build/day12
 rm day*.o
---
+++ 2019/day12.cpp
@@ -0,0 +1,118 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <regex>
+#include <tuple>
+
+#include "utils/input.hpp"
+
+struct Planet
+{
+    std::tuple<int, int, int> pos;
+    std::tuple<int, int, int> vel;
+
+    int &get_pos(const int i)
+    {
+        switch (i)
+        {
+        case 0:
+            return std::get<0>(pos);
+        case 1:
+            return std::get<1>(pos);
+        case 2:
+            return std::get<2>(pos);
+        default:
+            perror("Invalid index");
+            exit(EXIT_FAILURE);
+        }
+    };
+    int &get_vel(const int i)
+    {
+        switch (i)
+        {
+        case 0:
+            return std::get<0>(vel);
+        case 1:
+            return std::get<1>(vel);
+        case 2:
+            return std::get<2>(vel);
+        default:
+            perror("Invalid index");
+            exit(EXIT_FAILURE);
+        }
+    };
+};
+
+std::vector<Planet> get_planets(const std::vector<std::string> &input)
+{
+    std::vector<Planet> planets;
+    std::regex pat{R"(<x=(-?\d+), y=(-?\d+), z=(-?\d+)>)"};
+    std::smatch match;
+
+    for (std::string line : input)
+    {
+        regex_search(line, match, pat);
+        planets.push_back({{std::stoi(match.str(1)), std::stoi(match.str(2)), std::stoi(match.str(3))}, {0, 0, 0}});
+    }
+
+    return planets;
+}
+
+void apply_gravity(std::vector<Planet> &planets)
+{
+    const int no_planets = planets.size();
+    for (int i = 0; i != no_planets; i++)
+    {
+        for (int j = i + 1; j != no_planets; j++)
+        {
+            for (int k = 0; k != 3; k++)
+            {
+                if (planets[i].get_pos(k) < planets[j].get_pos(k))
+                {
+                    planets[i].get_vel(k) += 1;
+                    planets[j].get_vel(k) -= 1;
+                }
+                else if (planets[i].get_pos(k) > planets[j].get_pos(k))
+                {
+                    planets[i].get_vel(k) -= 1;
+                    planets[j].get_vel(k) += 1;
+                }
+            }
+        }
+    }
+}
+
+void apply_velocity(std::vector<Planet> &planets)
+{
+    for (int i = 0; i != planets.size(); i++)
+    {
+        planets[i].get_pos(0) += planets[i].get_vel(0);
+        planets[i].get_pos(1) += planets[i].get_vel(1);
+        planets[i].get_pos(2) += planets[i].get_vel(2);
+    }
+}
+
+int get_energy(std::vector<Planet> &planets)
+{
+    int energy = 0;
+    for (Planet planet : planets)
+    {
+        energy +=
+            (std::abs(planet.get_pos(0)) + std::abs(planet.get_pos(1)) + std::abs(planet.get_pos(2))) * (std::abs(planet.get_vel(0)) + std::abs(planet.get_vel(1)) + std::abs(planet.get_vel(2)));
+    }
+    return energy;
+}
+
+int main()
+{
+    std::vector<std::string> input = read_input_vs("./input/12/12.txt");
+
+    std::vector<Planet> planets = get_planets(input);
+    for (int i = 0; i != 1000; i++)
+    {
+        apply_gravity(planets);
+        apply_velocity(planets);
+    }
+
+    std::cout << "Part 1: " << get_energy(planets) << std::endl;
+}
