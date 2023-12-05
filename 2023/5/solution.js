43d00c59181617698364cee8ede94e39bbe09514
Dagur Páll Ammendrup <dagurp@gmail.com>
2023-12-05 23:54:49 +0000

    2023 - 5

--- 2023/5/solution.js
+++ 2023/5/solution.js
@@ -1,6 +1,7 @@
 const fs = require("fs");
 const filename = "input.txt";
 const content = fs.readFileSync(process.cwd() + "/" + filename).toString();
+
 const example1 = [
   "seeds: 79 14 55 13",
   "",
@@ -37,108 +38,76 @@ const example1 = [
   "56 93 4",
 ].join("\n");
 
+function getSeeds(content) {
+  return content.split("\n")[0].split(": ")[1].split(" ").map(Number);
+}
 
-// seeds: 79 14 55 13
+function getMaps(content) {
+  const mapText = content.split("\n\n").slice(1);
+  return mapText.map((line) => {
+    const [name, specsText] = line.split(" map:\n");
+    return specsText.split("\n").map((line) => line.split(" ").map(Number));
+  });
+}
 
-// seed-to-soil map:
-// 50 98 2
-// 52 50 48
+function map(value, spec) {
+  for (let i = 0; i < spec.length; i++) {
+    const [dest, src, range] = spec[i];
+    if (value >= src && value < src + range) {
+      return dest + value - src;
+    }
+  }
+  return value;
+}
 
-// soil-to-fertilizer map:
-// 0 15 37
-// 37 52 2
-// 39 0 15
+function solve(content) {
+  const seeds = getSeeds(content);
+  const maps = getMaps(content);
 
-// fertilizer-to-water map:
-// 49 53 8
-// 0 11 42
-// 42 0 7
-// 57 7 4
+  const values = maps.reduce((vals, spec) => {
+    return vals.map((val) => map(val, spec));
+  }, seeds);
 
-// water-to-light map:
-// 88 18 7
-// 18 25 70
+  return Math.min.apply(this, values);
+}
 
-// light-to-temperature map:
-// 45 77 23
-// 81 45 19
-// 68 64 13
+function* generateVals(seeds) {
+  for (let i = 0; i < seeds.length / 2; i++) {
+    const start = seeds.shift();
+    const range = seeds.shift();
+    for (let j = start; j < start + range; j++) {
+      yield j;
+    }
+  }
+}
 
-// temperature-to-humidity map:
-// 0 69 1
-// 1 0 69
+function* loop(vals, spec) {
+  for (const val of vals) {
+    yield map(val, spec);
+  }
+}
 
-// humidity-to-location map:
-// 60 56 37
-// 56 93 4
+function solve2(content) {
+  const seeds = getSeeds(content);
+  let vals = generateVals(seeds);
+  const maps = getMaps(content);
 
-// The almanac starts by listing which seeds need to be planted: seeds 79, 14,
-// 55, and 13.
+  for (let i = 0; i < maps.length; i++) {
+    const spec = maps[i];
+    vals = loop(vals, spec);
+  }
 
-// The rest of the almanac contains a list of maps which describe how to convert
-// numbers from a source category into numbers in a destination category. That
-// is, the section that starts with seed-to-soil map: describes how to convert a
-// seed number (the source) to a soil number (the destination). This lets the
-// gardener and his team know which soil to use with which seeds, which water to
-// use with which fertilizer, and so on.
+  let min = vals.next().value;
+  for (const val of vals) {
+    min = Math.min(min, val);
+  }
 
-// Rather than list every source number and its corresponding destination number
-// one by one, the maps describe entire ranges of numbers that can be converted.
-// Each line within a map contains three numbers: the destination range start,
-// the source range start, and the range length.
+  return min;
+}
 
-// Consider again the example seed-to-soil map:
-
-// 50 98 2
-// 52 50 48
-// The first line has a destination range start of 50, a source range start of 98,
-// and a range length of 2. This line means that the source range starts at 98 and
-// contains two values: 98 and 99. The destination range is the same length, but
-// it starts at 50, so its two values are 50 and 51. With this information, you
-// know that seed number 98 corresponds to soil number 50 and that seed number
-// 99 corresponds to soil number 51.
-
-// The second line means that the source range starts at 50 and contains 48 values:
-// 50, 51, ..., 96, 97. This corresponds to a destination range starting at 52
-// and also containing 48 values: 52, 53, ..., 98, 99. So, seed number 53
-// corresponds to soil number 55.
-
-// Any source numbers that aren't mapped correspond to the same destination number.
-// So, seed number 10 corresponds to soil number 10.
-
-// So, the entire list of seed numbers and their corresponding soil numbers looks
-// like this:
-
-// seed  soil
-// 0     0
-// 1     1
-// ...   ...
-// 48    48
-// 49    49
-// 50    52
-// 51    53
-// ...   ...
-// 96    98
-// 97    99
-// 98    50
-// 99    51
-// With this map, you can look up the soil number required for each initial seed
-// number:
-
-// Seed number 79 corresponds to soil number 81.
-// Seed number 14 corresponds to soil number 14.
-// Seed number 55 corresponds to soil number 57.
-// Seed number 13 corresponds to soil number 13.
-// The gardener and his team want to get started as soon as possible, so they'd like
-// to know the closest location that needs a seed. Using these maps, find the lowest
-// location number that corresponds to any of the initial seeds. To do this, you'll
-// need to convert each seed number through other categories until you can find its
-// corresponding location number. In this example, the corresponding types are:
-
-// Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
-// Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
-// Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
-// Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.
-// So, the lowest location number in this example is 35.
-
-// What is the lowest location number that corresponds to any of the initial seed numbers?
+console.log({
+  example1: solve(example1),
+  solution1: solve(content),
+  "example1-2": solve2(example1),
+  solution2: solve2(content), // Very slow
+});
